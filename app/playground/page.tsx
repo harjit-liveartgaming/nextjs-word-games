a9682e106e5ba16e246730e39e91e5e50da0339c
Harjit Randhawa <harjit@liveartgaming.com>
2024-08-21 13:13:02 -0700

    fixed line spacing

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,9 @@
 import { fetchChallenge } from "../wordle/db/data";
 
 export default function Page() {
-
-    
-    const joinChallenge = async (id: string) => {
-        let challenge = await fetchChallenge(id);
-        //initializeGame(challenge.word, challenge.attempts);
-        console.log(challenge.id);
-    }
+  const joinChallenge = async (id: string) => {
+    let challenge = await fetchChallenge(id);
+    //initializeGame(challenge.word, challenge.attempts);
+    console.log(challenge.id);
+  }
 }
--- app/wordle/components/forms.tsx
+++ app/wordle/components/forms.tsx
@@ -1,17 +1,17 @@
-export function InputFieldComponent(props:{label: string, inputType: 'text' | 'number' , onSubmit:()=>{}, onChange: (e:React.FormEvent<HTMLInputElement>)=>{}, value: string | number}){
-    return (
-        <div className="h-full">
-            <div className='justify-center items-center flex w-full'>
+export function InputFieldComponent(props: { label: string, inputType: 'text' | 'number', onSubmit: () => {}, onChange: (e: React.FormEvent<HTMLInputElement>) => {}, value: string | number }) {
+  return (
+    <div className="h-full">
+      <div className='justify-center items-center flex w-full'>
 
 
-            <label className="p-2 text-align-center ">{props.label}</label>
-            <input className="p-2 w-2/5 border-2" type={props.inputType} value={props.value}
-                onChange={(e: React.FormEvent<HTMLInputElement>) =>{props.onChange(e)}}>
+        <label className="p-2 text-align-center ">{props.label}</label>
+        <input className="p-2 w-2/5 border-2" type={props.inputType} value={props.value}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => { props.onChange(e) }}>
 
-            </input>
-            <button className="p-2 w-1/5 border-2"
-                onClick={()=>{props.onSubmit()}}>Next</button>
-            </div>
-        </div>
-    )
+        </input>
+        <button className="p-2 w-1/5 border-2"
+          onClick={() => { props.onSubmit() }}>Next</button>
+      </div>
+    </div>
+  )
 }
